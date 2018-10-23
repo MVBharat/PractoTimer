123a376404b5e2f2035815e8782f114a6a062a5b
MVBharat <manebharat85@gmail.com>
2018-10-23 16:31:45 +0530

    fix issue and Add horn tune for after stop watch

--- app/src/main/java/com/example/bharat/practotimer/MainActivity.java
+++ app/src/main/java/com/example/bharat/practotimer/MainActivity.java
@@ -1,25 +1,43 @@
   package com.example.bharat.practotimer;
 
+
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
   public class MainActivity extends AppCompatActivity {
       SeekBar timerSeekBar;
+      TextView timerTextView;
+      public  void updateTimer(int secondsLeft){
+          int minutes = (int)secondsLeft / 60 ;
+          int seconds = secondsLeft - minutes * 60;
+
+          String secondString=  Integer.toString(seconds);
+          if (seconds <= 9){
+              secondString = "0" + secondString;
+          }
+          timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+      }
       public void controlTimer(View view){
-          new CountDownTimer(timerSeekBar.getProgress() * 1000,1000) {
+          new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100,1000) {
               @Override
               public void onTick(long millisUntilFinished) {
-                        
+                updateTimer((int)millisUntilFinished / 1000);
               }
 
               @Override
               public void onFinish() {
-
+                timerTextView.setText("0:00");
+                Log.d("finished", "Done!");
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                mediaPlayer.start();
               }
           };
       }
@@ -29,23 +47,17 @@ import android.widget.TextView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timerTextView = (TextView)findViewById(R.id.timerTextView);
         timerSeekBar = (SeekBar)findViewById(R.id.timerSeekBar);
-        final TextView timerTextView = (TextView)findViewById(R.id.timerTextView);
+        Button controllerButton = (Button)findViewById(R.id.controllerButton);
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
+        controlTimer(findViewById(R.id.controllerButton));
 
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minutes = (int)progress / 60 ;
-                int seconds = progress - minutes * 60;
-
-                String secondString=  Integer.toString(seconds);
-                if (secondString == "0"){
-                    secondString = "00";
-                }
-                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
-
+                updateTimer(progress);
             }
 
             @Override
